6a3a555a27a4270af6b10f75c05bfce894b8a167
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-03 15:28:34 +0200

    Initializing Project ...

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ExpenseItem from "./components/expense-item/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+        <ExpenseItem/>
+    </div>
+  );
+}
+
+export default App;
