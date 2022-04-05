b1aa5925cfe25e327f4e96b594628b3be55a5b9b
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-05 19:03:31 +0200

    Managing a List of Users via State ...

--- 8-complete-practice-project/src/App.js
+++ 8-complete-practice-project/src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 import AddUser from "./components/users/user-form/AddUser";
 import UserList from "./components/users/user-list/UserList";
 
 function App() {
-  return (
-    <div className="App">
-      <AddUser/>
-      <UserList users={[]}/>
-    </div>
-  );
+    const [userList, setUserList] = useState([])
+
+    const addUserHandler = (username, age) => {
+        setUserList(prevState =>  {
+            return [
+                ...prevState,
+                { id: Math.random() ,name: username, age: age }
+            ]
+        })
+    }
+    return (
+        <div className="App">
+            <AddUser onAddUser={addUserHandler}/>
+            <UserList users={userList}/>
+        </div>
+    );
 }
 
 export default App;
--- 8-complete-practice-project/src/components/users/user-form/AddUser.jsx
+++ 8-complete-practice-project/src/components/users/user-form/AddUser.jsx
@@ -19,6 +19,7 @@ const AddUser = (props) => {
             return
         }
         console.log(enteredUsername, enteredAge)
+        props.onAddUser(enteredUsername, enteredAge)
         // Resetting Fields
         setEnteredUsername('')
         setEnteredAge('')
