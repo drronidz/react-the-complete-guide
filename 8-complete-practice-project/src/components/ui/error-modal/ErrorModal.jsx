68afcd4e94ab9e5219b0061b4aa8e405ef443959
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-05 19:25:06 +0200

    Managing the Error State ...

--- 8-complete-practice-project/src/components/ui/error-modal/ErrorModal.jsx
+++ 8-complete-practice-project/src/components/ui/error-modal/ErrorModal.jsx
@@ -6,7 +6,7 @@ import Button from "../button/Button";
 const ErrorModal = props => {
     return (
         <div>
-            <div className="backdrop"/>
+            <div className="backdrop" onClick={props.onConfirm}/>
             <Card className="modal">
                 <header className="header">
                     <h2>{props.title}</h2>
@@ -15,7 +15,7 @@ const ErrorModal = props => {
                     <p>{props.message}</p>
                 </div>
                 <footer className="actions">
-                    <Button>Okay</Button>
+                    <Button onClick={props.onConfirm}>Okay</Button>
                 </footer>
             </Card>
         </div>
--- 8-complete-practice-project/src/components/users/user-form/AddUser.jsx
+++ 8-complete-practice-project/src/components/users/user-form/AddUser.jsx
@@ -5,18 +5,26 @@ import Card from "../../ui/card/Card";
 import ErrorModal from "../../ui/error-modal/ErrorModal";
 
 
-
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('')
     const [enteredAge, setEnteredAge] = useState('')
+    const [error, setError] = useState()
 
     const addUserHandler = (event) => {
         event.preventDefault()
         // Validating Fields
         if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: 'Invalid Input',
+                message: 'Please enter valid name and age (non-empty values).'
+            })
             return
         }
         if (+enteredAge < 1) {
+            setError({
+                title: 'Invalid age',
+                message: 'Please enter valid age (> 0).'
+            })
             return
         }
         console.log(enteredUsername, enteredAge)
@@ -34,9 +42,13 @@ const AddUser = (props) => {
         setEnteredAge(event.target.value)
     }
 
+    const errorHandler = () => {
+        setError(null)
+    }
+
     return (
         <div>
-            <ErrorModal title="An error occured!" message="Something went wrong!"/>
+            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
             <Card className="input">
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>
