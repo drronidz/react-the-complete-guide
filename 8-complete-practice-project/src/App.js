179feef5b58ec77edca6a510895527545af48c6e
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-05 17:13:30 +0200

    Setting Up project & Adding a User Component ...

---
+++ 8-complete-practice-project/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import AddUser from "./components/users/AddUser";
+
+function App() {
+  return (
+    <div className="App">
+      <AddUser/>
+    </div>
+  );
+}
+
+export default App;
