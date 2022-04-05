5fa74caf11bbf1e87d8601d63503b6d26f3b0a57
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-05 17:42:01 +0200

    Adding Resetting & Validation Logic ...

--- 8-complete-practice-project/src/components/users/AddUser.jsx
+++ 8-complete-practice-project/src/components/users/AddUser.jsx
@@ -9,7 +9,17 @@ const AddUser = (props) => {
 
     const addUserHandler = (event) => {
         event.preventDefault()
+        // Validating Fields
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            return
+        }
+        if (+enteredAge < 1) {
+            return
+        }
         console.log(enteredUsername, enteredAge)
+        // Resetting Fields
+        setEnteredUsername('')
+        setEnteredAge('')
     }
 
     const usernameChangeHandler = event => {
@@ -24,9 +34,9 @@ const AddUser = (props) => {
         <Card className="input">
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" onChange={usernameChangeHandler}/>
+                <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" onChange={ageChangeHandler}/>
+                <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
                 <Button type="submit">Add user</Button>
             </form>
         </Card>
