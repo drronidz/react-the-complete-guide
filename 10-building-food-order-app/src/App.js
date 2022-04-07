281782c0f14db0392f8352359d952b48221d08e2
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-07 17:16:25 +0200

    Managing Cart & Modal State (Showing & Hiding)...

--- 10-building-food-order-app/src/App.js
+++ 10-building-food-order-app/src/App.js
@@ -1,14 +1,24 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 import Header from "./components/Layout/header/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/cart/Cart";
 
 
 function App() {
+    const [cartIsVisible, setCartIsVisible] = useState(false)
+
+    const showCartHandler = () => {
+        setCartIsVisible(true)
+    }
+
+    const hideCartHandler = () => {
+        setCartIsVisible(false)
+    }
+
     return (
         <Fragment>
-            <Cart/>
-            <Header/>
+            {cartIsVisible && <Cart onHideCart={hideCartHandler}/>}
+            <Header onShowCart={showCartHandler}/>
             <main>
                 <Meals/>
             </main>
