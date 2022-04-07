f69d6db156a415c99b844a83dbc3fdd2df1c296d
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-07 17:59:03 +0200

    Working with Refs & ForwardRefs ...

--- 10-building-food-order-app/src/components/Meals/meal-item-form/MealItemForm.js
+++ 10-building-food-order-app/src/components/Meals/meal-item-form/MealItemForm.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from './MealItemForm.module.css'
 import Input from "../../UI/input/Input";
 
 
  const MealItemForm = props => {
+     const [amountIsValid, setAmountIsValid] = useState(true)
+     const amountInputRef = useRef()
+
+     const submitHandler = event => {
+         event.preventDefault()
+
+         const enteredAmount = amountInputRef.current.value
+         const enteredAmountNumber = +enteredAmount
+
+         if (enteredAmount.trim().length === 0 ||
+             enteredAmountNumber < 1 ||
+             enteredAmountNumber > 5)
+         {
+             setAmountIsValid(false)
+             return
+         }
+
+         props.onAddToCart(enteredAmountNumber)
+     }
+
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <Input
+                ref={amountInputRef}
                 label='Amount'
                 input={{
                 id: 'amount',
@@ -17,6 +38,7 @@ import Input from "../../UI/input/Input";
                 defaultValue: '1'
             }}/>
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1 to 5)</p>}
         </form>
     )
  }
--- 10-building-food-order-app/src/components/Meals/meal-item/MealItem.js
+++ 10-building-food-order-app/src/components/Meals/meal-item/MealItem.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {useContext} from "react";
 
 import classes from './MealItem.module.css'
 import MealItemForm from "../meal-item-form/MealItemForm";
+import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
+    const cartContext = useContext(CartContext)
+
     const price = `$${props.price.toFixed(2)}`
+
+    const addToCartHandler = amount => {
+        cartContext.addItemToCart({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        })
+    }
 
     return (
         <li className={classes.meal}>
@@ -14,7 +26,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm/>
+                <MealItemForm onAddToCart={addToCartHandler}/>
             </div>
         </li>
     )
