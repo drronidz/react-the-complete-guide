884d7ceb2cb5de1f5e45dbe1072c295c9cbd5638
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-07 17:32:45 +0200

    Using the Context in HeaderCartButton badge ...

--- 10-building-food-order-app/src/components/Layout/header/header-cart-button/HeaderCartButton.js
+++ 10-building-food-order-app/src/components/Layout/header/header-cart-button/HeaderCartButton.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React ,{ useContext } from "react";
+import CartContext from "../../../../store/cart-context";
+
 import classes from './HeaderCartButton.module.css'
 import CartIcon from "../../../Cart/cart-icon/CartIcon";
 
 const HeaderCartButton = props => {
+    const cartContext = useContext(CartContext)
+
+    const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => {
+        return currentNumber + item.amount
+    }, 0)
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon/>
             </span>
             <span>Your Cart</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     )
 }
