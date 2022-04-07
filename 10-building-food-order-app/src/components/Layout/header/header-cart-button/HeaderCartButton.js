ee64d1e0fd7d46ac797318df8980f28e6842bdb6
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-08 01:20:41 +0200

    Using the useEffect Hook

--- 10-building-food-order-app/src/components/Layout/header/header-cart-button/HeaderCartButton.js
+++ 10-building-food-order-app/src/components/Layout/header/header-cart-button/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React ,{ useContext } from "react";
+import React, {useContext, useEffect, useState} from "react";
 import CartContext from "../../../../store/cart-context";
 
 import classes from './HeaderCartButton.module.css'
@@ -6,13 +6,32 @@ import CartIcon from "../../../Cart/cart-icon/CartIcon";
 
 const HeaderCartButton = props => {
     const cartContext = useContext(CartContext)
+    const {items} = cartContext
+    const [btnIsBumped, setBtnIsBumped] = useState(false)
 
-    const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => {
+    const numberOfCartItems = items.reduce((currentNumber, item) => {
         return currentNumber + item.amount
     }, 0)
 
+    useEffect(() => {
+        if (items.length === 0) {
+            return
+        }
+        setBtnIsBumped(true)
+
+        const timer = setTimeout(() => {
+            setBtnIsBumped(false)
+        }, 300)
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [items])
+
+    const btnClasses = `${classes.button} ${btnIsBumped ? classes.bump: ''}`
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon/>
             </span>
