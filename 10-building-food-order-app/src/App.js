46c753f5a61062d63f363cbfbfda7b8c7fb844be
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-04-07 16:48:54 +0200

    Adding a Modal via ReactPortal ...

--- 10-building-food-order-app/src/App.js
+++ 10-building-food-order-app/src/App.js
@@ -1,17 +1,19 @@
 import React, {Fragment} from "react";
 import Header from "./components/Layout/header/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/cart/Cart";
 
 
 function App() {
-  return (
-    <Fragment>
-      <Header/>
-      <main>
-          <Meals/>
-      </main>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <Cart/>
+            <Header/>
+            <main>
+                <Meals/>
+            </main>
+        </Fragment>
+    );
 }
 
 export default App;
--- 10-building-food-order-app/src/components/Cart/cart/Cart.js
+++ 10-building-food-order-app/src/components/Cart/cart/Cart.js
@@ -1,18 +1,21 @@
 import React from "react";
 import classes from './Cart.module.css'
+import Modal from "../../UI/modal/Modal";
 
 
 
 const Cart = props =>{
-    const cartItems = [
-        { id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }
-    ].map((item) => <li> {item.name} </li>)
+    const cartItems = (
+        <ul className={classes['cart-items']}>
+            {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }]
+                .map((item) =>
+                    <li> {item.name} </li>)}
+        </ul>
+    )
 
     return (
-        <div>
-            <ul className={classes['cart-items']}>
-                {cartItems}
-            </ul>
+        <Modal>
+            {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>35.62</span>
@@ -21,7 +24,7 @@ const Cart = props =>{
                 <button className={classes['button--alt']}>Close</button>
                 <button className={classes.button}>Order</button>
             </div>
-        </div>
+        </Modal>
     )
 }
 
